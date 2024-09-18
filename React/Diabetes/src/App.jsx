b6707c0c66c72bc8da59a9f73a348eca1248f25c
PetJs <peterfagoroye76@gmail.com>
2024-09-18 18:59:26 +0100

    modifying the styles

--- React/Diabetes/src/App.jsx
+++ React/Diabetes/src/App.jsx
@@ -62,7 +62,7 @@ function App() {
     };
 
     try {
-      const response = await fetch('http://localhost:8000/predict', {
+      const response = await fetch('https://diabetes-fastapi.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
